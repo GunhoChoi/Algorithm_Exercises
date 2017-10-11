48ad258947a6019b80b9aca93e769476b5478a6e
Choi Gunho <ghc0311@naver.com>
2017-10-11 17:04:21 +0900

    Update Longest_Palindrom.py

--- Longest_Palindrom.py
+++ Longest_Palindrom.py
@@ -1,3 +1,5 @@
+# https://www.welcomekakao.com/learn/challenge_codes/85
+
 def longest_palindrom(s):
     result=0
     length = len(s)
@@ -12,3 +14,12 @@ def longest_palindrom(s):
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
 print(longest_palindrom("토마토맛토마토"))
 print(longest_palindrom("토마토맛있어"))
+
+
+'''
+가장 짧은 답은 아래와 같았음
+
+def longest_palindrom(s):
+    # 함수를 완성하세요
+    return len(s) if s[::-1] == s else max(longest_palindrom(s[:-1]), longest_palindrom(s[1:]))
+'''
